187978796515b29c058a432607cb7b4d45788a4a
sebastianbocul <seba23495@gmail.com>
2023-02-21 23:05:31 +0100

    Created compose UI

--- app/src/main/java/com/sebix/graphqlcountriesapp/MainActivity.kt
+++ app/src/main/java/com/sebix/graphqlcountriesapp/MainActivity.kt
@@ -8,16 +8,28 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.sebix.graphqlcountriesapp.presentation.CountriesScreen
+import com.sebix.graphqlcountriesapp.presentation.CountriesViewModel
 import com.sebix.graphqlcountriesapp.ui.theme.GraphQlCountriesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GraphQlCountriesAppTheme {
-
+                val viewModel = hiltViewModel<CountriesViewModel>()
+                val state by viewModel.state.collectAsState()
+                CountriesScreen(
+                    state = state,
+                    onSelectedCountry = { viewModel.selectCountry(it) },
+                    onDismissCountryDialog = viewModel::dismissCountryDialog)
             }
         }
     }
