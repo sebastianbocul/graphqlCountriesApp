99655b596c8f4da23df0c8ab5282ae1490c6e722
sebastianbocul <seba23495@gmail.com>
2023-02-21 22:06:11 +0100

    Added di, country mapper

---
+++ app/src/main/java/com/sebix/graphqlcountriesapp/CountriesApp.kt
@@ -0,0 +1,7 @@
+package com.sebix.graphqlcountriesapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CountriesApp:Application()
---
+++ app/src/main/java/com/sebix/graphqlcountriesapp/data/CountryMappers.kt
@@ -0,0 +1,28 @@
+package com.sebix.graphqlcountriesapp.data
+
+import com.sebix.CountriesQuery
+import com.sebix.CountryQuery
+import com.sebix.graphqlcountriesapp.domain.DetailedCountry
+import com.sebix.graphqlcountriesapp.domain.SimpleCountry
+
+fun CountryQuery.Country.toDetailedCountry(): DetailedCountry {
+    return DetailedCountry(
+        code = code,
+        name = name,
+        emoji = emoji,
+        capital = capital ?: "No capital",
+        currency = currency ?: "No currency",
+        languages = languages.mapNotNull {
+            it.name
+        },
+        continent = continent.name
+    )
+}
+fun CountriesQuery.Country.toSimpleCountry(): SimpleCountry {
+    return SimpleCountry(
+        code = code,
+        name = name,
+        emoji = emoji,
+        capital = capital ?: "No capital"
+    )
+}
