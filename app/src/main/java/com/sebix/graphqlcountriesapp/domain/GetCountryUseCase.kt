798c8038ea5d8a6e5971278e25cb1db98db5f44b
sebastianbocul <seba23495@gmail.com>
2023-02-21 22:05:13 +0100

    Created use cases and view model

---
+++ app/src/main/java/com/sebix/graphqlcountriesapp/domain/GetCountryUseCase.kt
@@ -0,0 +1,11 @@
+package com.sebix.graphqlcountriesapp.domain
+
+class GetCountryUseCase(
+    private val countryClient: CountryClient
+) {
+    suspend fun execute(code: String): DetailedCountry? {
+        return countryClient
+            .getCountry(code)
+
+    }
+}
